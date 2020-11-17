b180c13d9adaea028b7b873125c18fc0590fb31d
Anson <1072476988@qq.com>
2020-11-17 13:09:05 +0800

    [Lingzhen Kong] feat: finish practice1.

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb;
 
+import static java.util.concurrent.TimeUnit.DAYS;
+
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +15,12 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int year = date.getYear();
+    LocalDate labourDay = LocalDate.of(year, 5, 1);
+    if (date.isBefore(labourDay)){
+      return ChronoUnit.DAYS.between(date,labourDay);
+    }else {
+      return ChronoUnit.DAYS.between(date,labourDay.withYear(year+1));
+    }
   }
 }
